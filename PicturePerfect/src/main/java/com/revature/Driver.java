661c094cb385f67a08d124db64efe1926e6ed170
djbogar <46689626+djbogar@users.noreply.github.com>
2019-04-02 19:14:32 -0400

    adding war compatiblity

--- PicturePerfect/src/main/java/com/revature/Driver.java
+++ PicturePerfect/src/main/java/com/revature/Driver.java
@@ -2,9 +2,19 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.amazonaws.services.appstream.model.Application;
 
 	@SpringBootApplication
-	public class Driver {
+	public class Driver extends SpringBootServletInitializer{
+		
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(Application.class);
+		}
+		
 		public static void main(String[] args) {
 			SpringApplication.run(Driver.class, args);
 		}
