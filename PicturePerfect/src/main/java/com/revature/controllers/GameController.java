c4a5a3c37de82bac3b1043db6c1cdd673dab191c
Kevin Tran Huu <kevin.tranhuu@gmail.com>
2019-04-03 22:51:19 -0400

    Implemented game deletion logic, admin ban functionality

--- PicturePerfect/src/main/java/com/revature/controllers/GameController.java
+++ PicturePerfect/src/main/java/com/revature/controllers/GameController.java
@@ -33,62 +33,59 @@ public class GameController {
 	GameService gameService;
 	@Autowired
 	UserService userService;
-	
-	@GetMapping(produces=MediaType.APPLICATION_JSON_VALUE)
-	public List<Game> findAllGames(){		return gameService.findAllGames();
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Game> findAllGames() {
+		return gameService.findAllGames();
 	}
-	
-	@GetMapping(value = "/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Game findGameById(@PathVariable("id") Integer id) {
 		return gameService.findGameById(id);
 	}
-	
-	@GetMapping(value = "/{resume}/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(value = "/{resume}/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Game> findGameByResume(@PathVariable("resume") String resume, @PathVariable("id") Integer id) {
 		return gameService.findGamesByTurn(id);
 	}
-	
-	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Game> addGame(@Valid @RequestBody Game game){
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Game> addGame(@Valid @RequestBody Game game) {
 		List<User> users = userService.getAllUsers();
 		List<User> filledList = new ArrayList<>();
-		for(User u : game.getUsers()) {
-			for(User us : users) {
-				if(u.getuId() != null && u.getuId().equals(us.getuId())) {
+		for (User u : game.getUsers()) {
+			for (User us : users) {
+				if (u.getuId() != null && u.getuId().equals(us.getuId())) {
 					filledList.add(us);
 				}
-				if(u.getUsername() != null && u.getUsername().equals(us.getUsername())) {
+				if (u.getUsername() != null && u.getUsername().equals(us.getUsername())) {
 					filledList.add(us);
 					game.setTurn(us.getuId());
 				}
 			}
 		}
-		if(filledList.size() < 2) {
+		if (filledList.size() < 2) {
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		game.setUsers(filledList);
 		return new ResponseEntity<>(gameService.addGame(game), HttpStatus.CREATED);
 	}
-	
+
 	@PutMapping("/{id}")
-	public ResponseEntity<Game> updateGame(@PathVariable("id") Integer id, @Valid @RequestBody Game game){
+	public ResponseEntity<Game> updateGame(@PathVariable("id") Integer id, @Valid @RequestBody Game game) {
 		Game temp = gameService.findGameById(id);
-		if(temp == null) {
+		if (temp == null) {
 			return new ResponseEntity<Game>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<>(gameService.updateGame(game), HttpStatus.OK);
-//		for (Game g : games) {
-//			if(g.getG_id() == id) {
-//				game.setG_id(id);
-//				return new ResponseEntity<>(gameService.updateGame(game), HttpStatus.OK);
-//			}
-//		}
-//		return new ResponseEntity<Game>(HttpStatus.NOT_FOUND);
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public Game deleteGame(@PathVariable("id") Integer id) {
-		Game game = new Game(id);
-		return gameService.deleteGame(game);
+	public ResponseEntity<Game> deleteGame(@PathVariable("id") Integer id) {
+		Game game =  gameService.findGameById(id);
+		if (game == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(gameService.deleteGame(game), HttpStatus.ACCEPTED);
 	}
 }
